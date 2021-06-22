2692dc0272bceb0b2574e11653d4bc3f20e65d9b
Rishabh <rishabhkuletha@hotmail.com>
2021-06-22 15:51:06 +0530

    Add files via upload
    simple_heads_tails

---
+++ heads_tails.py
@@ -0,0 +1,19 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+head_or_tail = random.randint(0,1)
+
+if head_or_tail == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
+
